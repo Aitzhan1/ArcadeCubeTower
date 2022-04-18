8069c05d9ad9bfd4a7b49c4f774ae9a309cbf572
Aitzhan0846 <aitmuhan0@gmail.com>
2022-04-18 07:43:33 +0600

    Added UI

--- Assets/Scripts/UIButtons.cs
+++ Assets/Scripts/UIButtons.cs
@@ -3,8 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class UIButtons : MonoBehaviour
 {
-   public void Restart()
+    public void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void OpenInsta()
+    {
+        Application.OpenURL("https://www.instagram.com/aitzhan_slk/");
     }
 }
