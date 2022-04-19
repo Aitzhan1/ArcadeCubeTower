e6a7fbf265c4020a47eb4e8114d5a1c4bc1a4eed
Aitzhan <aitmuhan0@gmail.com>
2022-04-19 18:06:06 +0600

    Camera position

--- Assets/Scripts/SpawnCubes.cs
+++ Assets/Scripts/SpawnCubes.cs
@@ -1,21 +1,27 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.EventSystems;
 
 public class SpawnCubes : MonoBehaviour
 {
     private CubePos nowCube = new CubePos(0, 1, 0);
     public float cubeChangePlaceSpeed = 0.5f;
+    private float camToYPosition, camMoveSpeed=2f;
 
     public Transform cubeToPlace;
+    private Transform mainCam;
 
     public GameObject cubeToCreate, allCubes;
+    public GameObject[] canvasStartPage;
 
     private Rigidbody allCubesRb;
 
-    private bool IsLose;
+    private bool IsLose, firstCube;
+
     private Coroutine showCubePlace;
 
+    private int prevCountMaxHorizontal;
     private List<Vector3> allCubesPositions = new List<Vector3>
     {
         new Vector3(0,0,0),
@@ -29,37 +35,62 @@ public class SpawnCubes : MonoBehaviour
         new Vector3(-1,0,1),
         new Vector3(1,0,-1),
     };
+    public Color[] bgColor;
+    private Color toCameraColor;
     private void Start()
     {
+        toCameraColor = Camera.main.backgroundColor;
+        mainCam = Camera.main.transform;
+        camToYPosition = 5.9f + nowCube.y - 1f;
+
         allCubesRb = allCubes.GetComponent<Rigidbody>();
         showCubePlace = StartCoroutine(ShowCubePlace());
     }
 
     private void Update()
     {
-        if ((Input.GetMouseButtonDown(0) || Input.touchCount > 0) && cubeToPlace!=null && allCubes!=null)
-        {
+        if ((Input.GetMouseButtonDown(0) || Input.touchCount > 0) && cubeToPlace != null && allCubes != null && !EventSystem.current.IsPointerOverGameObject())
+            if ((Input.GetMouseButtonDown(0) || Input.touchCount > 0) && cubeToPlace != null && allCubes != null && !EventSystem.current.IsPointerOverGameObject())
+            {
 #if !UNITY_EDITOR
             if (Input.GetTouch(0).phase != TouchPhase.Began)
                 return;
 #endif
 
-          GameObject newCube = Instantiate(cubeToCreate, cubeToPlace.position, Quaternion.identity);
+                if (EventSystem.current.IsPointerOverGameObject())
+                    return;
 
-            newCube.transform.SetParent(allCubes.transform);
-            nowCube.setVector(cubeToPlace.position);
-            allCubesPositions.Add(nowCube.getVector());
+                if (!firstCube)
+                {
+                    firstCube = true;
+                    foreach (GameObject obj in canvasStartPage)
+                    {
+                        Destroy(obj);
+                    }
+                }
+                GameObject newCube = Instantiate(cubeToCreate, cubeToPlace.position, Quaternion.identity);
 
-            allCubesRb.isKinematic = true;
-            allCubesRb.isKinematic = false;
-            SpawnPosition();
-        }
+                newCube.transform.SetParent(allCubes.transform);
+                nowCube.setVector(cubeToPlace.position);
+                allCubesPositions.Add(nowCube.getVector());
+
+                allCubesRb.isKinematic = true;
+                allCubesRb.isKinematic = false;
+                SpawnPosition();
+                MoveCameraChangeBg();
+            }
         if (!IsLose && allCubesRb.velocity.magnitude > 0.1f)
         {
             Destroy(cubeToPlace.gameObject);
             IsLose = true;
             StopCoroutine(showCubePlace);
         }
+
+        mainCam.localPosition = Vector3.MoveTowards(mainCam.localPosition,
+            new Vector3(mainCam.localPosition.x, camToYPosition, mainCam.localPosition.z), camMoveSpeed * Time.deltaTime);
+
+        if (Camera.main.backgroundColor != toCameraColor)
+            Camera.main.backgroundColor = Color.Lerp(Camera.main.backgroundColor, toCameraColor, Time.deltaTime / 1.5f);
     }
     IEnumerator ShowCubePlace()
     {
@@ -76,25 +107,30 @@ public class SpawnCubes : MonoBehaviour
         List<Vector3> positions = new List<Vector3>();
 
         if (IsPositionEmpty(new Vector3(nowCube.x + 1, nowCube.y, nowCube.z))
-            && nowCube.x+1!=cubeToPlace.position.x)
+            && nowCube.x + 1 != cubeToPlace.position.x)
             positions.Add(new Vector3(nowCube.x + 1, nowCube.y, nowCube.z));
         if (IsPositionEmpty(new Vector3(nowCube.x - 1, nowCube.y, nowCube.z))
             && nowCube.x - 1 != cubeToPlace.position.x)
             positions.Add(new Vector3(nowCube.x - 1, nowCube.y, nowCube.z));
-        if (IsPositionEmpty(new Vector3(nowCube.x, nowCube.y+1, nowCube.z))
+        if (IsPositionEmpty(new Vector3(nowCube.x, nowCube.y + 1, nowCube.z))
             && nowCube.y + 1 != cubeToPlace.position.y)
-            positions.Add(new Vector3(nowCube.x, nowCube.y+1, nowCube.z));
+            positions.Add(new Vector3(nowCube.x, nowCube.y + 1, nowCube.z));
         if (IsPositionEmpty(new Vector3(nowCube.x, nowCube.y - 1, nowCube.z))
             && nowCube.y - 1 != cubeToPlace.position.y)
             positions.Add(new Vector3(nowCube.x, nowCube.y - 1, nowCube.z));
-        if (IsPositionEmpty(new Vector3(nowCube.x, nowCube.y, nowCube.z+1))
+        if (IsPositionEmpty(new Vector3(nowCube.x, nowCube.y, nowCube.z + 1))
             && nowCube.z + 1 != cubeToPlace.position.z)
-            positions.Add(new Vector3(nowCube.x, nowCube.y, nowCube.z+1));
-        if (IsPositionEmpty(new Vector3(nowCube.x, nowCube.y, nowCube.z-1))
+            positions.Add(new Vector3(nowCube.x, nowCube.y, nowCube.z + 1));
+        if (IsPositionEmpty(new Vector3(nowCube.x, nowCube.y, nowCube.z - 1))
             && nowCube.z - 1 != cubeToPlace.position.z)
-            positions.Add(new Vector3(nowCube.x, nowCube.y, nowCube.z-1));
+            positions.Add(new Vector3(nowCube.x, nowCube.y, nowCube.z - 1));
 
-        cubeToPlace.position = positions[Random.Range(0,positions.Count)];
+        if (positions.Count > 1)
+            cubeToPlace.position = positions[Random.Range(0, positions.Count)];
+        else if (positions.Count == 0)
+            IsLose = true;
+        else
+            cubeToPlace.position = positions[0];
     }
     private bool IsPositionEmpty(Vector3 targetPos)
     {
@@ -107,6 +143,36 @@ public class SpawnCubes : MonoBehaviour
                 return false;
         }
         return true;
+    }
+
+    private void MoveCameraChangeBg()
+    {
+        int maxX = 0;
+        int maxY = 0; 
+        int maxZ = 0;
+        int maxHor;
+        foreach (Vector3 pos in allCubesPositions)
+        {
+            if(Mathf.Abs((int)pos.x)>maxX)
+                maxX =(int)pos.x;
+            if ((int)pos.y > maxY)
+                maxY = (int)pos.y;
+            if (Mathf.Abs((int)pos.z) > maxZ)
+                maxZ = (int)pos.z;
+        }
+        camToYPosition = 5.9f + nowCube.y - 1f;
+        maxHor = maxX > maxZ ? maxX : maxZ;
+        if (maxHor % 3 == 0 && prevCountMaxHorizontal!=maxHor)
+        {
+            mainCam.localPosition -= new Vector3(0, 0, 2.5f);
+            prevCountMaxHorizontal = maxHor;
+        }
+        if (maxY >= 7)
+            toCameraColor = bgColor[2];
+        else if (maxY >= 5)
+            toCameraColor = bgColor[1];
+        else if (maxY >= 2)
+            toCameraColor = bgColor[0];
     }
 }
 struct CubePos
